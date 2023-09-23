8487a1b98d04976b0e45c0ff4e540de65f36de66
Connor Boyle <connor.bo@gmail.com>
2023-09-22 19:12:22 -0700

    Implement async merge sort with tokio

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use rand::Rng;
 use rand::rngs::ThreadRng;
 use std::time::Instant;
 use std::thread;
+use tokio;
+use futures::future::{BoxFuture, FutureExt};
 
 fn is_sorted<T: PartialOrd>(list: &Vec<T>) -> bool {
     let mut previous_item_option: Option<&T> = None;
@@ -62,22 +64,22 @@ fn merge_sort<T: PartialOrd + Clone>(list: &[T]) -> Vec<T>{
     }
 }
 
-fn parallel_merge_sort<T: PartialOrd + Clone + Send + Sync + 'static>(list: &[T], parallelism_depth: u8) -> Vec<T> {
+fn thread_merge_sort<T: PartialOrd + Clone + Send + Sync + 'static>(list: &[T], parallelism_depth: u8) -> Vec<T> {
     let list_copy: Arc<[T]> = Arc::from(list.to_vec().into_boxed_slice());
-    _parallel_merge_sort(list_copy, 0, list.len(), parallelism_depth)
+    _thread_merge_sort(list_copy, 0, list.len(), parallelism_depth)
 }
 
-fn _parallel_merge_sort<T: PartialOrd + Clone + Send + Sync + 'static>(list: Arc<[T]>, begin: usize, end: usize, parallelism_depth: u8) -> Vec<T> {
+fn _thread_merge_sort<T: PartialOrd + Clone + Send + Sync + 'static>(list: Arc<[T]>, begin: usize, end: usize, parallelism_depth: u8) -> Vec<T> {
     if end - begin == 1 {
         list.to_vec()
     } else if parallelism_depth > 0 {
         let pivot: usize = (begin + end) / 2;
 
         let first_ref = Arc::clone(&list);
-        let first_thread = thread::spawn(move || _parallel_merge_sort(first_ref, begin, pivot, parallelism_depth - 1));
+        let first_thread = thread::spawn(move || _thread_merge_sort(first_ref, begin, pivot, parallelism_depth - 1));
 
         let second_ref = Arc::clone(&list);
-        let second_thread = thread::spawn(move || _parallel_merge_sort(second_ref, pivot, end, parallelism_depth - 1));
+        let second_thread = thread::spawn(move || _thread_merge_sort(second_ref, pivot, end, parallelism_depth - 1));
 
         merge(&first_thread.join().unwrap(), &second_thread.join().unwrap())
     } else {
@@ -85,20 +87,41 @@ fn _parallel_merge_sort<T: PartialOrd + Clone + Send + Sync + 'static>(list: Arc
     }
 }
 
+fn async_merge_sort<T: PartialOrd + Clone + Send + Sync>(list: &[T]) -> BoxFuture<Vec<T>> {
+    if list.len() == 1 {
+        async move { list.to_vec() }.boxed()
+    } else {
+        let pivot = list.len() / 2;
+        let first_thread = async_merge_sort(&list[0..pivot]);
+        let second_thread = async_merge_sort(&list[pivot..list.len()]);
+
+        async move {
+            merge(&first_thread.await, &second_thread.await)
+        }.boxed()
+    }
+}
+
 fn random_range(rng: &mut ThreadRng, n: usize, lower: usize, upper: usize) -> Vec<usize> {
     (0..n).map(|_| rng.gen_range(lower..upper)).collect::<Vec<usize>>()
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut rng = rand::thread_rng();
-    let list = random_range(&mut rng, 50_000, 0, 100);
+    let list = random_range(&mut rng, 5_000_000, 0, 100);
     assert!(!is_sorted(&list), "`list` is sorted! This can technically occur by chance, but should be very unlikely if `n` is sufficiently high.");
 
     let start = Instant::now();
-    let parallel_merge_sorted = parallel_merge_sort(&list, 2);
-    assert!(is_sorted(&parallel_merge_sorted));
+    let thread_merge_sorted = thread_merge_sort(&list, 2);
+    assert!(is_sorted(&thread_merge_sorted));
     let duration = start.elapsed();
-    println!("Successfully sorted using parallel merge sort in {:#?}!", duration);
+    println!("Successfully sorted using thread merge sort in {:#?}!", duration);
+
+    let start = Instant::now();
+    let async_merge_sorted = async_merge_sort(&list).await;
+    assert!(is_sorted(&async_merge_sorted));
+    let duration = start.elapsed();
+    println!("Successfully sorted using async merge sort in {:#?}!", duration);
 
     let start = Instant::now();
     let merge_sorted = merge_sort(&list);
@@ -106,11 +129,11 @@ fn main() {
     let duration = start.elapsed();
     println!("Successfully sorted using merge sort in {:#?}!", duration);
 
-    let start = Instant::now();
-    let bubble_sorted = bubble_sort(&list);
-    assert!(is_sorted(&bubble_sorted));
-    let duration = start.elapsed();
-    println!("Successfully sorted using bubble sort in {:?}!", duration);
+    // let start = Instant::now();
+    // let bubble_sorted = bubble_sort(&list);
+    // assert!(is_sorted(&bubble_sorted));
+    // let duration = start.elapsed();
+    // println!("Successfully sorted using bubble sort in {:?}!", duration);
 }
 
 #[test]
@@ -147,6 +170,14 @@ fn test_merge_sort() {
 fn test_parallel_merge_sort() {
     let list = vec![2, 5, 10, 3, 4, 1, 6, 9, 8, 7];
     assert!(!is_sorted(&list));
-    let sorted_list = parallel_merge_sort(&list, 2);
+    let sorted_list = thread_merge_sort(&list, 2);
+    assert_eq!(sorted_list, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+}
+
+#[test]
+fn test_async_merge_sort() {
+    let list = vec![2, 5, 10, 3, 4, 1, 6, 9, 8, 7];
+    assert!(!is_sorted(&list));
+    let sorted_list = tokio_test::block_on(async_merge_sort(&list));
     assert_eq!(sorted_list, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 }
